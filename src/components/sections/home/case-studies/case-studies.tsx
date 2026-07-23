8a9764d98e39df7308fbe75ddedd17d30c63a151
Anton <mazzur54@gmail.com>
2026-07-23 11:42:25 +0300

    feat: implement case studies component with responsive design and remove unused assets

--- src/components/sections/home/case-studies/case-studies.tsx
+++ src/components/sections/home/case-studies/case-studies.tsx
@@ -3,11 +3,14 @@ import { Badge } from '@/components/ui/badge';
 import { Button } from '@/components/ui/button';
 import { Typography } from '@/components/ui/typography';
 
+import CaseStudiesItem from './components/case-studies-item';
+import { caseStudiesData } from './data';
+
 export function CaseStudies() {
   return (
-    <section className='flex w-full flex-col pt-15.5 lg:px-10.5 lg:pt-55'>
-      <ComponentContainer className='flex flex-col gap-y-45'>
-        <div className='flex flex-col items-center px-4 text-center lg:px-0'>
+    <section className='flex w-full flex-col pt-15.5 lg:pt-55'>
+      <ComponentContainer className='flex flex-col gap-y-17 lg:gap-y-45'>
+        <div className='flex flex-col items-start px-4 lg:items-center lg:px-0 lg:text-center'>
           <Badge title='Case studies' className='mb-7.5 w-fit lg:mb-10' />
 
           <div className='flex flex-col items-start justify-between gap-4.75 lg:mb-12.5 lg:flex-row lg:gap-10'>
@@ -16,8 +19,9 @@ export function CaseStudies() {
               <span className='text-foreground/50'>technical precision with strategic thinking to solve complex business challenges.</span>
             </Typography>
           </div>
-          <Button className='w-43'>View more</Button>
+          <Button className='hidden w-43 lg:block'>View more</Button>
         </div>
+        <CaseStudiesItem item={caseStudiesData[0]} />
       </ComponentContainer>
     </section>
   );
--- src/components/sections/home/case-studies/components/case-studies-item.tsx
+++ src/components/sections/home/case-studies/components/case-studies-item.tsx
@@ -1,5 +1,44 @@
-function CaseStudiesItem() {
-  return <div className='flex items-center'></div>;
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Icon } from '@/components/ui/icon';
+import { Typography } from '@/components/ui/typography';
+import { cn } from '@/lib/utils';
+
+import type { CaseStudiesItem as CaseStudiesItemData } from '../data';
+interface CaseStudiesItemProps {
+  item: CaseStudiesItemData;
+}
+
+function CaseStudiesItem({ item }: CaseStudiesItemProps) {
+  const { title, description, tools, image, href } = item;
+  return (
+    <div className={cn('flex flex-col-reverse items-center gap-x-5 gap-y-7', 'lg:flex-row 2xl:gap-x-45')}>
+      <div className='flex flex-col gap-y-3 px-4 lg:gap-y-11.5 lg:px-10.5'>
+        <Typography variant='h2'>{title}</Typography>
+        <Typography variant='body2'>{description}</Typography>
+        <div className='mt-3 flex flex-wrap gap-2 lg:mt-0'>
+          {tools.map((tool) => (
+            <Badge key={tool} className='text-18!'>
+              {tool}
+            </Badge>
+          ))}
+        </div>
+      </div>
+      <Link href={href} className='lg:max-w-auto relative w-145 max-w-full lg:min-w-1/2'>
+        <Image src={image} alt={title} width={945} height={684} className='w-full rounded-2xl' />
+        <Button
+          variant='icon'
+          size='icon-md'
+          className='text-foreground lg:bg-foreground pointer-events-none absolute right-2 bottom-2 bg-white lg:right-12.5 lg:bottom-12.5 lg:text-white'
+        >
+          <Icon icon='ArrowUpRight' className='size-4.5' />
+        </Button>
+      </Link>
+    </div>
+  );
 }
 
 export default CaseStudiesItem;
