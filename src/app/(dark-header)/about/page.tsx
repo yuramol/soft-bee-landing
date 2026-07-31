eb8368632b51d8cbaa47b53ef54d7d89a1c3e925
vitalekp <vitalekpushkarov@gmail.com>
2026-07-31 10:09:45 +0300

    feat: add about page

--- src/app/(dark-header)/about/page.tsx
+++ src/app/(dark-header)/about/page.tsx
@@ -1,20 +1,12 @@
-import { ComponentContainer } from '@/components/layout';
-import { AboutHero } from '@/components/sections/about';
-import { AboutUs, Careers, Services, Team } from '@/components/sections/home';
+import { AboutHero, Founders } from '@/components/sections/about';
+import { AboutUs, Careers, Team } from '@/components/sections/home';
 
 export default function AboutPage() {
   return (
     <>
       <AboutHero />
       <AboutUs />
-      <div className='bg-muted relative pb-30.5 md:pb-26.75'>
-        <ComponentContainer>
-          <div className='relative z-20 w-full overflow-x-clip rounded-lg bg-white md:rounded-2xl'>
-            <Services />
-            {/* <Tools /> */}
-          </div>
-        </ComponentContainer>
-      </div>
+      <Founders />
       <Team hideCoFounders />
       <Careers className='z-10 -mb-10 bg-transparent md:-mb-10' />
     </>
--- src/components/sections/about/index.ts
+++ src/components/sections/about/index.ts
@@ -1,1 +1,2 @@
 export { AboutHero } from './about-hero';
+export { Founders } from './founders/founders';
