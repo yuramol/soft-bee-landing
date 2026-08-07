a77b829c9db05922f831cb4e5586d865e254ee91
vitalekp <vitalekpushkarov@gmail.com>
2026-08-07 09:03:01 +0300

    feat: add nav-dropdown

--- src/constants/navigation.ts
+++ src/constants/navigation.ts
@@ -26,7 +26,14 @@ export const FOOTER_LINKS = [
 export const MAIN_NAV_LINKS = [
   { label: 'Home', href: '/' },
   { label: 'Services', href: '/services' },
-  { label: 'About', href: '/about' },
+  {
+    label: 'About',
+    href: '#',
+    subLinks: [
+      { label: 'Company', href: '/company' },
+      { label: 'Careers', href: '/careers' }
+    ]
+  },
   { label: 'Case studies', href: '/case-studies' },
   { label: 'Insights', href: '/insights' }
 ];
