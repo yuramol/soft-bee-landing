7c6686a8ebcdeb7ed4ebe2f3488cf6090cd6053e
vitalekp <vitalekpushkarov@gmail.com>
2026-08-13 16:59:09 +0300

    feat: add hero section

--- src/app/(light-header)/insights/page.tsx
+++ src/app/(light-header)/insights/page.tsx
@@ -1,7 +1,11 @@
+import { Hero } from '@/components/sections/home';
+
+import heroContent from '@/components/sections/insights/hero/content.json';
+
 export default function InsightsPage() {
   return (
-    <main className='text-brand-black flex min-h-screen flex-col items-center justify-center bg-white p-24'>
-      <h1 className='text-48 font-bold'>Insights</h1>
-    </main>
+    <>
+      <Hero titleSegments={heroContent.titleSegments} description={heroContent.description} />
+    </>
   );
 }
