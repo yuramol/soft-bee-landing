77f9e54faed96208c57fe15b64ef738de5a93428
Anton <mazzur54@gmail.com>
2026-09-10 09:34:16 +0300

    revert dynamic imports

--- src/app/(light-header)/page.tsx
+++ src/app/(light-header)/page.tsx
@@ -1,17 +1,12 @@
 import nextDynamic from 'next/dynamic';
 
 import { ComponentContainer } from '@/components/layout';
-import { AboutUs, Hero } from '@/components/sections/home';
+import { AboutUs, Careers, CaseStudies, Hero, Services, Team, Testimonials } from '@/components/sections/home';
 
 const VideoWrapper = nextDynamic(() => import('@/components/sections/home/video-wrapper').then((module) => module.VideoWrapper));
-const Services = nextDynamic(() => import('@/components/sections/home/services').then((module) => module.Services));
 const Tools = nextDynamic(() => import('@/components/sections/home/tools').then((module) => module.Tools));
-const Team = nextDynamic(() => import('@/components/sections/home/team').then((module) => module.Team));
-const Careers = nextDynamic(() => import('@/components/sections/home/careers').then((module) => module.Careers));
-const CaseStudies = nextDynamic(() => import('@/components/sections/home/case-studies').then((module) => module.CaseStudies));
 const SmartEstimation = nextDynamic(() => import('@/components/sections/home/smart-estimation').then((module) => module.SmartEstimation));
 const Awards = nextDynamic(() => import('@/components/sections/home/awards').then((module) => module.Awards));
-const Testimonials = nextDynamic(() => import('@/components/sections/home/testimonials').then((module) => module.Testimonials));
 
 export const dynamic = 'force-static';
 
